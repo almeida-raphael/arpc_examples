2fafacc3838c7d3feeeb50417af9dbd0a3a747e4
Ericson Fogo Soares <ericson.ds999@gmail.com>
2021-07-01 20:36:38 -0300

    Serialization metrics collection for gRPC

--- cmd/metric_collection_examples/serialization/grpc/main.go
+++ cmd/metric_collection_examples/serialization/grpc/main.go
@@ -26,55 +26,55 @@ func main() {
 	value := utils.Atoi(os.Getenv("VALUE"))
 	path := "serialization_results/Protobuffers/%s_.json"
 
-	var data proto.Message = &typebinary.Request{Binary: []byte(utils.GenerateString(value))}
+	var data proto.Message = &typebinary.TypeBinary{Binary: []byte(utils.GenerateString(value))}
 	err := utils.ExtractGRPCSerializationMetrics(data, trials, fmt.Sprintf(path, "binary"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data = &typebool.Request{Bool: rand.Float32() >= 0.5}
+	data = &typebool.TypeBool{Bool: rand.Float32() >= 0.5}
 	err = utils.ExtractGRPCSerializationMetrics(data, trials, fmt.Sprintf(path, "boolean"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data = &typefloat32.Request{Float32: rand.Float32()}
+	data = &typefloat32.TypeFloat32{Float32: rand.Float32()}
 	err = utils.ExtractGRPCSerializationMetrics(data, trials, fmt.Sprintf(path, "float32"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data = &typefloat64.Request{Float64: rand.Float64()}
+	data = &typefloat64.TypeFloat64{Float64: rand.Float64()}
 	err = utils.ExtractGRPCSerializationMetrics(data, trials, fmt.Sprintf(path, "float64"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data = &typeint32.Request{Int32: int32(rand.Uint32())}
+	data = &typeint32.TypeInt32{Int32: int32(rand.Uint32())}
 	err = utils.ExtractGRPCSerializationMetrics(data, trials, fmt.Sprintf(path, "int32"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data = &typeint64.Request{Int64: int64(rand.Uint64())}
+	data = &typeint64.TypeInt64{Int64: int64(rand.Uint64())}
 	err = utils.ExtractGRPCSerializationMetrics(data, trials, fmt.Sprintf(path, "int64"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data = &typetext.Request{Text: utils.GenerateString(value)}
+	data = &typetext.TypeText{Text: utils.GenerateString(value)}
 	err = utils.ExtractGRPCSerializationMetrics(data, trials, fmt.Sprintf(path, "string"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data = &typeuint32.Request{Uint32: rand.Uint32()}
+	data = &typeuint32.TypeUInt32{Uint32: rand.Uint32()}
 	err = utils.ExtractGRPCSerializationMetrics(data, trials, fmt.Sprintf(path, "int32"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data = &typeuint64.Request{Uint64: rand.Uint64()}
+	data = &typeuint64.TypeUInt64{Uint64: rand.Uint64()}
 	err = utils.ExtractGRPCSerializationMetrics(data, trials, fmt.Sprintf(path, "int64"))
 	if err != nil {
 		log.Fatal(err)
